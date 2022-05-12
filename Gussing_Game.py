d26e088fe594f52974ffb4a54b76418429c72cad
MahmoudKhalid22 <103892504+MahmoudKhalid22@users.noreply.github.com>
2022-05-12 07:13:17 +0200

    Add files via upload

---
+++ Gussing_Game.py
@@ -0,0 +1,10 @@
+#it makes an input function that print a message what is the fouvrite food and wait the user to input
+favourite_food = input ("What is my favourite food?")
+# if the user wrote meat it print the message Yep! So amazing 
+if favourite_food == "meat":
+    print("Yep! So amazing")
+#if the uset did not write meat it print Yuck! That is not it     
+else:
+    print("Yuck! That's not it")
+# regardeless the input of the user it print the message Thanks for playing    
+print("Thanks for playing!")        
