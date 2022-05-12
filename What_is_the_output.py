6dd52f5813fa701f92e77ed3cbf65468abe74b2f
MahmoudKhalid22 <103892504+MahmoudKhalid22@users.noreply.github.com>
2022-05-12 07:11:45 +0200

    Add files via upload

---
+++ What_is_the_output.py
@@ -0,0 +1,27 @@
+#what is the output?
+
+print("Challenge 1:") 
+
+#a message for the user 
+message = "This is going to be tricky;" 
+# it makes a variables that stores a string ("This is going to be tricky;")
+Message = "Very tricky!"
+# it makes a variables that stores a string ("Very tricky!")
+
+print(message) 
+
+#Perform mathematical operation 
+result = 2 ** 3 # it makes a variables that stores an int (2 power 3 (8))
+print ("2 ** 3 = ", result) 
+
+result = 5 - 3 
+#print("5 - 3 = ",result) 
+print("Challenge completed!")
+
+
+#it prints a phrase 'challenge 1:'
+#it prints the string that be stored in message varible ("This is going to be tricky")
+#it print a string (2 ** 3 = ) then print the value of result (8)  
+#it changes the value of reslut to (5 - 3 (2))
+#it does not print the value of result because it is a comment 
+#it prints the string "Challenge completed"
